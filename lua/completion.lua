cb9158a56118b6023881db041bc6c2119cb9f0d0
Garrett Leber <garrett@leberra.com>
2025-06-09 19:39:14 -0400

    auto-show documentation

--- lua/completion.lua
+++ lua/completion.lua
@@ -5,6 +5,7 @@ require("blink.cmp").setup({
   appearance = {
     nerd_font_variant = 'mono'
   },
+  completion = { documentation = { auto_show = true } },
   signature = { enabled = true, },
   sources = {
     default = { 'lsp', 'path', 'snippets', 'buffer' },
