ace35d1918fb7a6fe5e44ed4e8499e111da94a3c
Garrett Leber <garrett@leberra.com>
2025-06-09 21:12:44 -0400

    fix: remove native autocomplete since we're using blink

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -6,12 +6,3 @@ vim.lsp.enable({
   "luals",
   "nixd"
 })
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
-})
