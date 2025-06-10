19d7c83bd3b269d4ada1fe9ef10fc6c5d6f25db9
Garrett Leber <garrett@leberra.com>
2025-06-10 14:28:46 -0400

    don't use luasnip for now

--- lua/tcpkump/plugins/completion.lua
+++ lua/tcpkump/plugins/completion.lua
@@ -95,22 +95,6 @@ require('blink.cmp').setup({
     },
   },
 
-  -- Snippet configuration (for LuaSnip integration)
-  -- snippets = {
-  --   expand = function(snippet)
-  --     require('luasnip').lsp_expand(snippet)
-  --   end,
-  --   active = function(filter)
-  --     if filter and filter.direction then
-  --       return require('luasnip').jumpable(filter.direction)
-  --     end
-  --     return require('luasnip').in_snippet()
-  --   end,
-  --   jump = function(direction)
-  --     require('luasnip').jump(direction)
-  --   end,
-  -- },
-
   -- Additional features
   signature = {
     enabled = true,
